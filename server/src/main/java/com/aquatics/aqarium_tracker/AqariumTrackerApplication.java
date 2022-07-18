a78433a9432d592ed7d29154d8c031c474853cd4
sujan <sujangurung10@gmail.com>
2022-07-18 10:43:40 +0100

    initisl commit bsckend :)

---
+++ server/src/main/java/com/aquatics/aqarium_tracker/AqariumTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.aquatics.aqarium_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqariumTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqariumTrackerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/aquatics/aqarium_tracker/AqariumTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquatics.aqarium_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqariumTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
